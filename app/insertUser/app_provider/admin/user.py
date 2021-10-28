915d2abbd8534c67ce54e8d7d5615b7414222456
vampire <sepahisiavash@gmail.com>
2021-10-28 15:39:23 +0330

    add user data type

--- app/insertUser/app_provider/admin/user.py
+++ app/insertUser/app_provider/admin/user.py
@@ -1,9 +1,44 @@
 from core.app_provider.api.get import site_connection
 from core.config.Config import main_add_user_url, user_timeout
+from core.model.DataType import user_add_data
 
 
 def insert_user(f_name, l_name, email, phone, password):
-    payload = {"fname": str(f_name), "lname": str(l_name), "email": str(email), "phone": str(phone),
-               "password": str(password), "groupId": "6", "verified": "1"}
+    payload = get_data(f_name, l_name, email, phone, password)
 
     site_connection(main_add_user_url, user_timeout * 2, data=payload)
+
+
+def get_data(f_name, l_name, email, phone, password):
+    data_temp = dict(user_add_data)
+    key = sorted(list(data_temp.keys()), key=key_order)
+
+    data_temp[key[0]] = f_name  # for fname
+    data_temp[key[1]] = l_name  # for lname
+    data_temp[key[2]] = email  # for email
+    data_temp[key[3]] = phone  # for phone
+    data_temp[key[4]] = password  # for password
+    return data_temp
+
+
+def key_order(key):
+    import difflib
+    app_order = ["fname", "lname", "email", "phone", "password"]
+
+    key = difflib.get_close_matches(key, app_order)
+
+    if len(key) != 0:
+        if key[0] == "fname":
+            return 1
+        elif key[0] == "lname":
+            return 2
+        elif key[0] == "email":
+            return 3
+        elif key[0] == "phone":
+            return 4
+        elif key[0] == "password":
+            return 5
+        else:
+            return 0
+    else:
+        return 0
