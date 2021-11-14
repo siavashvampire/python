4e75cab8feb3382f67550f1ffb8676c8c35a5f3e
Yazdan <yazdan.arman@gmail.com>
2021-11-14 12:16:16 +0330

    add some code to list branch in test
    not working

--- yazdan_test.py
+++ yazdan_test.py
@@ -1,2 +1,25 @@
 # TODO: bayad beshe az table khasi az data base backup begire v to folder haye moshakhas berize v bayad data archive ro tike tike konim
+# TODO: jnjimjmmijmi
+# import QtQuick
+from PyQt5.QtWidgets import QListWidget
 
+Item {
+    width: 200; height: 250
+
+    ListModel {
+        id: myModel
+        ListElement { type: "Dog"; age: 8 }
+        ListElement { type: "Cat"; age: 5 }
+    }
+
+    Component {
+        id: myDelegate
+        Text { text: type + ", " + age }
+    }
+
+    ListView {
+        anchors.fill: parent
+        model: myModel
+        delegate: myDelegate
+    }
+}
