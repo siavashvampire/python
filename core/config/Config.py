280f7de7b3db2fc5d5681987a90f3bf36d4a7a8c
vampire <sepahisiavash@gmail.com>
2021-11-28 12:53:56 +0330

    fix resource

--- core/config/Config.py
+++ core/config/Config.py
@@ -12,7 +12,10 @@ os.makedirs(resource_path(config_path), exist_ok=True)
 config_db_path = resource_path(config_path + config_db_name)
 config_db = TinyDB(config_db_path).table(config_table_name)
 
-sAll = config_db.all()[0]
+try:
+    sAll = config_db.all()[0]
+except:
+    print("config not found in {link} with table {table}".format(link=config_db_path, table=config_table_name))
 # sAll = ConfigTB.get(doc_id=1)
 
 if not len(config_db):
