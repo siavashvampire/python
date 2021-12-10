2731ffcb68ca8fe84e495fc88b39c5b728965c90
siavash sepahi <sepahisiavash@gmail.com>
2021-12-10 15:35:36 +0330

    add whats app bot not complete

---
+++ test.py
@@ -0,0 +1,14 @@
+import pywhatkit
+from datetime import datetime, timedelta
+from MainCode import path
+
+now = datetime.now() + timedelta(seconds=75)
+
+text = "با  سلام خدمت شما" + "\n" + "این پیام توسط نرم افزار اتوماسیون صنعتی شرکت مرصاد برای شما ارسال میگردد" + "\n" \
+       + "ادامه دادن این برنامه به شما محول گردیده" + "\n" + "لطفا در سریع ترین زمان به کامل کردن این سیستم بپردازید"
+
+pywhatkit.sendwhatmsg_instantly("+989171129092", text, 15, True, 6)
+# pywhatkit.sendwhatmsg("+989171129092", text, now.hour, now.minute, 15, True, 6)
+# pywhatkit.sendwhats_image("+989171129092", path + "core/theme/pic/pic/MERSAD_Logo.png", "test aks", 15, True, 6)
+# pywhatkit.sendwhatmsg_to_group("HwypPTAYoxa8jsmqJQi3cM", text, now.hour, now.minute, 15, True, 6)
+# https://web.whatsapp.com/accept?code=HwypPTAYoxa8jsmqJQi3cM
